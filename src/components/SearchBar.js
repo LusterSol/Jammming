454ea582544d2c7017ed11f0749c9a26b203dcbe
Luster <ribera.sebastian@gmail.com>
2023-10-24 19:58:52 -0400

    searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const SearchBar = () => {
+    return (
+        <>
+            <div className='input-wrapper'>
+                
+            </div>
+        </>
+    )
+};
